d0061aac0e3eea08b5e75c08bbe59409c1ffb5aa
Максим <drakondr.video@gmail.com>
2024-07-10 20:10:46 +0700

    fix tabulation

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,52 +1,53 @@
-import { NavLink } from "react-router-dom"
-import user from "./../../img/user.svg"
-import time from "./../../img/time.svg"
-import basket from "./../../img/basket.svg"
-import exit from "./../../img/exit.svg"
-import etrance from "./../../img/etrance.svg"
-import "./NavBar.css"
-const NavBar = ({isLogged, logout}) => (
-	<>
-		<nav>
-			<div className='container'>
-				<NavLink to="/">
-					<div className="logo"/>
-				</NavLink>
-				
-				<div className="links">
-					<div>
-						<NavLink to={isLogged ? "/profile" : "/auth"}>
-							<div className="link">
-								<img src={user} alt="" />
-								<span>Профиль</span>
-							</div>
-						</NavLink>
-						<NavLink to="/orders">
-							<div className="link">
-								<img src={time} alt="" />
-								<span>Заказы</span>
-							</div>
-						</NavLink>
-					</div>
-					<div>
-						<NavLink to="/basket">
-							<div className="link">
-								<img src={basket} alt="" />
-								<span>Корзина</span>
-							</div>
-						</NavLink>
-						<NavLink to={isLogged ? "/" : "/auth"} >
-							<div className="link" onClick={() => {isLogged ? logout() : ""}}>
-								<img src={isLogged ? exit : etrance} alt="" />
-								{isLogged ? <span>Выйти</span> : <span>Войти</span>}
-							</div>
-						</NavLink>
-					</div>
-				</div>
-			</div>
-		</nav>
-		<hr />
-	</>
-)
+import { NavLink } from "react-router-dom";
+import user from "./../../img/user.svg";
+import time from "./../../img/time.svg";
+import basket from "./../../img/basket.svg";
+import exit from "./../../img/exit.svg";
+import etrance from "./../../img/etrance.svg";
+import "./NavBar.css";
 
-export { NavBar }
+const NavBar = ({ isLogged, logout }) => (
+   <>
+      <nav>
+         <div className="container">
+            <NavLink to="/">
+               <div className="logo" />
+            </NavLink>
+
+            <div className="links">
+               <div>
+                  <NavLink to={isLogged ? "/profile" : "/auth"}>
+                     <div className="link">
+                        <img src={user} alt="" />
+                        <span>Профиль</span>
+                     </div>
+                  </NavLink>
+                  <NavLink to="/orders">
+                     <div className="link">
+                        <img src={time} alt="" />
+                        <span>Заказы</span>
+                     </div>
+                  </NavLink>
+               </div>
+               <div>
+                  <NavLink to="/basket">
+                     <div className="link">
+                        <img src={basket} alt="" />
+                        <span>Корзина</span>
+                     </div>
+                  </NavLink>
+                  <NavLink to={isLogged ? "/" : "/auth"}>
+                     <div className="link" onClick={() => { isLogged ? logout() : ""}}>
+                        <img src={isLogged ? exit : etrance} alt="" />
+                        {isLogged ? <span>Выйти</span> : <span>Войти</span>}
+                     </div>
+                  </NavLink>
+               </div>
+            </div>
+         </div>
+      </nav>
+      <hr />
+   </>
+);
+
+export { NavBar };
